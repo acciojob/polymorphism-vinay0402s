4d9f88c3c7b7eac1836afe91af06f409c6ce7043
vinayShende <vinayjshende@gmail.com>
2023-07-14 18:53:57 +0530

    code changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+
+    public static class product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args){
+        product obj = new product();
+        obj.product(1,2);
+        obj.product(1,2,3);
+        obj.product(1.0,2.0);
+    }
 }
