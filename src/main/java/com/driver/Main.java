75a8364479e7ddf7ae652710a94efd7bf1731d55
vinayShende <vinayjshende@gmail.com>
2023-07-14 19:45:44 +0530

    try with same code r

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,33 @@
 package com.driver;
 
 public class Main {
-    public static class product{
-        public int product(int x, int y) {
+
+    public static class Product{
+
+        public int product(int x,int y){
             return x*y;
         }
-        public int product(int x, int y, int z) {
+
+        public int product(int x,int y, int z){
             return x*y*z;
         }
-        public double product(double x, double y) {
+
+
+
+        public double product(double x, double y){
             return x*y;
         }
+
     }
 
     public static void main(String[] args){
-        product obj = new product();
-        obj.product(1,2);
-        obj.product(1,2,3);
-        obj.product(1.0,2.0);
+        Product p = new Product();
+        p.product(2,5);
+
+
+        p.product(2,5,3);
+
+
+        p.product(2.0,3.0);
     }
 }
